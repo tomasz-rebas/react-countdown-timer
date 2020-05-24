47401b918d84ac3a37f4f57fbd41bf2e236427c1
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-05-24 17:42:01 +0200

    Set default input values

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,12 +5,12 @@ function Form(props) {
         <form onSubmit={props.saveUserInput}>
             <div>
                 <h2>Form</h2>
-                <p>Type in data about the event.</p>
+                <p className="info">Type in data about your event</p>
             </div>
             <div className="input-container">
-                <input name="name" placeholder="Name"/><br/>
-                <input name="date" placeholder="Date (e.g. 12 Oct 2020)"/><br/>
-                <input name="time" placeholder="Time (e.g. 23:50)"/><br/>
+                <input name="name" placeholder="Name" defaultValue={props.eventData.name}/><br/>
+                <input name="date" placeholder="Date (e.g. 12 Oct 2020)" defaultValue={props.eventData.date}/><br/>
+                <input name="time" placeholder="Time (e.g. 23:50)" defaultValue={props.eventData.time}/><br/>
                 <button className="save-button">Save</button>
             </div>
         </form>
